0e02b8d47f9f47682962e10f96d8dd3209519c50
YazanTawila <infoyazantawila@gmail.com>
2024-11-29 19:54:46 +0300

    the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import './index.css'
 
 function App() {
-
-
   return (
-    <div>Hi</div>
+    <div>
+      
+    </div>
   )
 }
 
