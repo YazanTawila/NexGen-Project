38e9f23bc8e1f9a202722d2a1116e5ec236b1598
YazanTawila <infoyazantawila@gmail.com>
2024-12-27 20:27:23 +0300

    Add Infinite Scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
 import './index.css'
-
+import InfiniteScroll from './components/InfiniteScroll/InfiniteScroll'
+import scroll from './Data/data'
 
 function App() {
   return (
     <div>
-      
+      <InfiniteScroll items={scroll.follow} color="var(--secondary-color)"/>
     </div>
   )
 }
