454e544f96ae52ff4a5a5fffc0a76b8abff4ae0e
unknown <matarhanna1111@gmail.com>
2024-12-29 15:26:43 -0500

    navbar and footer

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../components/NavBar/NavBar'
+import Footer from '../components/Footer/Footer'
+
+export default function HomePage() {
+  return (
+    <div>
+        {/* <NavBar /> */}
+        <Footer/>
+    </div>
+  )
+}
