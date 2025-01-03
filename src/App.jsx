7bb92eff86cad067a6cdfc0bc680a4121627a7fd
Rana92100 <hrna921@gmail.com>
2025-01-03 09:23:50 +0300

    digital component in home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+// import React from 'react'
 import './App.css'
+import Digital1 from './components/Digital1/Digital1'
 import './index.css'
 
 
 function App() {
   return (
     <div>
-      
+      <Digital1/>
     </div>
   )
 }
