820dddfbf9bbb0188a7e9277c9b419196fe9f725
YazanTawila <infoyazantawila@gmail.com>
2025-01-03 23:53:23 +0300

    Add a new section

---
+++ src/components/ReasonsCardComponent/ReasonsCardComponent.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './ReasonsCardComponent.css'
+import rightArrow from '../../assets/images/right-arrow.svg'
+import ButtonTextandIcon from '../ButtonTextandIcon/ButtonTextandIcon'
+import ImageSrc from "../../assets/images/arrow.svg";
+import ButtonIcon from '../ButtonIcon/ButtonIcon';
+function ReasonsCardComponent({title,desc}) {
+    return (
+        <div className='YT-ReasonsCardComponent'>
+                    <h3 className='YT-ReasonsCardComponent-Title'>{title}</h3>
+                    <p className='YT-ReasonsCardComponent-Desc'>{desc}</p>
+                <ButtonTextandIcon
+                    hiedText={false}
+                    ShowButtonIcon={false}
+                    ImgSrc={ImageSrc}
+                    TextSpan={"Learn More"}
+                    backColor={"var(--secondary-color)"}
+                    iconStyle={'YT-BlackIcon'}
+                    buttonTextStyle={'YT-Button-Text-Style'}
+                    />
+        </div>
+    )
+}
+
+export default ReasonsCardComponent
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,11 +5,15 @@ import Testimonial from '../components/Testimonial/Testimonial'
 import InfiniteScroll from '../components/InfiniteScroll/InfiniteScroll'
 import scroll from '../Data/data'
 import Question from '../components/Question/Question'
+import ReasonsToChoose from '../components/ReasonsToChoose/ReasonsToChoose'
+import OurWorks from '../components/OurWorks/OurWorks'
 export default function HomePage() {
   return (
     <div>
         {/* <NavBar /> */}
-        <Question/>
+        <ReasonsToChoose/>
+        <OurWorks/>
+        {/* <Question/> */}
         <Testimonial/>
         <InfiniteScroll items={scroll.follow} color={'var(--secondary-color)'}/>
         {/* <Footer/> */}
