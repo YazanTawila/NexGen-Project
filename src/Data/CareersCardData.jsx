935e9ed7b50e755d94e07e6629cd27d46c3af6d6
YazanTawila <infoyazantawila@gmail.com>
2025-01-15 18:23:28 +0300

    edit section join our team at nexgen

--- src/Data/CareersCardData.jsx
+++ src/Data/CareersCardData.jsx
@@ -1,77 +1,40 @@
-import img9 from "../assets/images/arrow.svg";
-import iconImg from "../assets/images/Pipette.svg";
-import iconImg2 from "../assets/images/Smartphone.svg";
-import iconImg3 from "../assets/images/Widget.svg";
-import iconImg4 from "../assets/images/Suitcase.svg";
+import icon1 from "../assets/images/Pipette.svg";
+import icon2 from "../assets/images/Smartphone.svg";
+import icon3 from "../assets/images/Widget.svg";
+import icon4 from "../assets/images/Suitcase.svg";
 const CareersCardData = [
   {
-    icon: iconImg,
-    title: "WEB DESIGNER",
-    img: img9,
-    button: "View Details",
-    salaryTitle: "Salary:",
-    salary: "$45,000 - $60,000 per year",
-    experienceTitle: "Experience:",
-    experience: "+1 year",
-    deadlineTitle: "Deadline:",
-    deadline: "30/9/2025",
-    skillsTitle: "Skills",
-    skills:
-      "Proficiency in Adobe Creative Suite (Photoshop, Illustrator, etc.), strong understanding of UI/UX design principles, responsive design expertise.",
-    button2: "APPLY NOW",
+    Icon:icon1 ,
+    Text:'Web Designer' ,
+    Salary :'$45,000 - $60,000 per year' ,
+    Experience : '2+ years',
+    Deadline :'30/09/2025' ,
+    Desc:'Proficiency in Adobe Creative Suite (Photoshop, Illustrator, etc.), strong understanding of UI/UX design principles, responsive design expertise.'
   },
-
   {
-    icon: iconImg2,
-    title: "Mobile App Developer",
-    img: img9,
-    button: "View Details",
-    salaryTitle: "Salary:",
-    salary: "$55,000 - $75,000 per year",
-    experienceTitle: "Experience:",
-    experience: "+2 years",
-    deadlineTitle: "Deadline:",
-    deadline: "15/10/2025",
-    skillsTitle: "Skills",
-    skills:
-      "Proficiency in iOS or Android development (Swift, Kotlin), knowledge of mobile app design patterns, experience with mobile testing and debugging.",
-
-    button2: "APPLY NOW",
+    Icon:icon2 ,
+    Text:'Mobile App Developer' ,
+    Salary :'$55,000 - $75,000 per year' ,
+    Experience : '1+ year',
+    Deadline :'15/10/2025' ,
+    Desc:'Proficiency in iOS or Android development (Swift, Kotlin), knowledge of mobile app design patterns, experience with mobile testing and debugging.',
   },
-
   {
-    icon: iconImg3,
-    title: "Digital Marketing Specialist",
-    img: img9,
-    button: "View Details",
-    salaryTitle: "Salary:",
-    salary: "$50,000 - $65,000 per year",
-    experienceTitle: "Experience:",
-    experience: "+3 years",
-    deadlineTitle: "Deadline:",
-    deadline: "20/11/2025",
-    skillsTitle: "Skills",
-    skills:
-      "Proficiency in SEO, SEM, and social media marketing, experience with analytics tools (Google Analytics, etc.), content marketing expertise.",
-    button2: "APPLY NOW",
+    Icon:icon3 ,
+    Text:'Digital Marketing Specialist' ,
+    Salary :'$50,000 - $65,000 per year' ,
+    Experience : '3+ years',
+    Deadline :'20/11/2025' ,
+    Desc:'Proficiency in SEO, SEM, and social media marketing, experience with analytics tools (Google Analytics, etc.), content marketing expertise.',
   },
-
   {
-    icon: iconImg4,
-    title: "Project Manager",
-    img: img9,
-    button: "View Details",
-    salaryTitle: "Salary:",
-    salary: "$60,000 - $80,000 per year",
-    experienceTitle: "Experience:",
-    experience: "+5 years",
-    deadlineTitle: "Deadline:",
-    deadline: "05/12/2025",
-    skillsTitle: "Skills",
-    skills:
-      "Strong organizational and communication skills, ability to manage project timelines and resources, experience in Agile project management methodologies.",
-
-    button2: "APPLY NOW",
+    Icon:icon4 ,
+    Text:'Project Manager' ,
+    Salary :'$60,000 - $80,000 per year' ,
+    Experience : '5+ years',
+    Deadline :'05/12/2025' ,
+    Desc:'Strong organizational and communication skills, ability to manage project timelines and resources, experience in Agile project management methodologies.',
   },
+
 ];
 export default CareersCardData;
