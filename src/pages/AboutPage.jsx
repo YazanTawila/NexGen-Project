3d0a7b3aa496a95eb42c32af45c10b7d9ba17450
Layal-Jamal <layaljamal242@gmail.com>
2025-01-17 14:48:06 +0300

    Meet the brilliant section,Awards section

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import NavBar from '../components/NavBar/NavBar'
 import Testimonial from '../components/Testimonial/Testimonial'
-import InfiniteScroll from '../components/InfiniteScroll/InfiniteScroll'
-import scroll from '../Data/data'
-import Question from '../components/Question/Question'
-import Digital1 from '../components/Digital1/Digital1'
-import Footer from '../components/Footer/Footer'
+import BrilliantSection from '../components/BrilliantSection/BrilliantSection'
+import Award from '../components/Award/Award'
+ import InfiniteScroll from '../components/InfiniteScroll/InfiniteScroll'
+ import scroll from '../Data/data'
+ import Question from '../components/Question/Question'
+ import Digital1 from '../components/Digital1/Digital1'
+ import Footer from '../components/Footer/Footer'
 
 export default function AboutPage() {
   return (
     <div>
         <NavBar/>
-        <Testimonial/>
+      
+     <BrilliantSection/>
+    <Award/>
+    <Testimonial/>
         <Question/>
         <Digital1/>
         <InfiniteScroll items={scroll.follow} color={'var(--secondary-color)'}/>
