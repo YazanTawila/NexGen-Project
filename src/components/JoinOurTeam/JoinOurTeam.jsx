14926e453ee8086b17d79fdc3e082f45f74dddeb
unknown <matarhanna1111@gmail.com>
2025-01-17 10:14:19 -0500

    matar

--- src/components/JoinOurTeam/JoinOurTeam.jsx
+++ src/components/JoinOurTeam/JoinOurTeam.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './JoinOurTeam.css'
 import Tittel from '../Tittel/Tittel'
 import ImageSrc from "../../assets/images/arrow.svg";
 import JoinOurTeamCard from '../JoinOurTeamCard/JoinOurTeamCard';
 import CareersCardData from '../../Data/CareersCardData'
+import downArrow from '../../assets/images/down-arrow.png'
+import { div } from 'framer-motion/client';
 function JoinOurTeam() {
-
+ const [isSmallScreen, setIsSmallScreen] = useState(false);
+    const [showLastCard, setShowLastCard] = useState(false); 
+    useEffect(() => {
+      const handleResize = () => {
+        setIsSmallScreen(window.innerWidth <= 850);   
+      };
+      handleResize();  
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize); 
+    }, []);
   return (
     <div className='MA-JoinOurTeam'>
       <Tittel
@@ -17,7 +28,8 @@ function JoinOurTeam() {
         <div className='MA-JoinOurTeamCardDiv'>
             {CareersCardData?.map((e, index) => {
             return (
-              <JoinOurTeamCard
+              <div className={`MA-JoinOurTeamCard block1 ${isSmallScreen && index >=2 && !showLastCard ? 'hidden' : ''}`}>
+                <JoinOurTeamCard 
                 key={index}
                 Text={e.Text}
                 Icon={e.Icon}
@@ -26,8 +38,14 @@ function JoinOurTeam() {
                 Deadline={e.Deadline}
                 Desc={e.Desc}
               />
+              </div>
+              
             );
           })}
+           {isSmallScreen && !showLastCard && (
+                              <button className='mh-hiddenbtn' onClick={() => setShowLastCard(true)}>
+                               <img src={downArrow} alt="" /> VIEW ALL
+                              </button>)}
         </div>
     </div>
   )
