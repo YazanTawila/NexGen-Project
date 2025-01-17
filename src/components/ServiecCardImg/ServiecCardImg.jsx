db53e3c4d1082bf6c97ba69b7dbea67c32926432
hebamomar <hebamomar851@gmail.com>
2025-01-17 19:11:57 +0300

    update

--- src/components/ServiecCardImg/ServiecCardImg.jsx
+++ src/components/ServiecCardImg/ServiecCardImg.jsx
@@ -2,19 +2,13 @@ import "./ServiecCardImg.css";
 import ButtonTextandIcon from "../ButtonTextandIcon/ButtonTextandIcon";
 import Arrow from "../../assets/images/arrowblack.svg";
 import ImgSrc from "../../assets/images/arrow.svg";
-import StoriesIcon from "../StoriesIcon/StoriesIcon";
-export default function ServiecCardImg({ img1, img2, textheading }) {
+export default function ServiecCardImg({img1,img2,textheading}) {
   return (
     <>
-      <div className="hm-Container-img">
-        <div className="hm-seviesTiteteel">
-          <StoriesIcon
-            ShowText={false}
-            ShowImg={true}
-            Text={textheading}
-            styleheading={"hm-heading"}
-          />
-
+  
+     <div className="hm-Container-img">
+     <div className="hm-seviesTiteteel">
+          <h3 className="hm-heading">{textheading}</h3>
           <ButtonTextandIcon
             hiedText={false}
             ShowButtonIcon={false}
@@ -24,12 +18,12 @@ export default function ServiecCardImg({ img1, img2, textheading }) {
             ButtonText={"Hm-ButtonText"}
             iconStyle={"HM-button-icon"}
           />
-        </div>
-        <div className="hm-seviesImg">
-          <div className="hm-seviesImg1">
-            <img src={img1} className="hm-mark"></img>
-            <div className="hm-text">
-              <ButtonTextandIcon
+          </div >
+          <div className="hm-seviesImg">
+            <div className="hm-seviesImg1">
+             <img src={img1} className="hm-mark"></img>
+             <div className="hm-text">
+             <ButtonTextandIcon
                 bttonandiconstyle={"Hm-ButtonServies"}
                 hiedText={false}
                 ShowButtonIcon={false}
@@ -38,12 +32,12 @@ export default function ServiecCardImg({ img1, img2, textheading }) {
                 ButtonText={"Hm-Servies-text"}
                 iconStyle={"HM-button-img"}
               />
+             </div>
             </div>
-          </div>
-          <div className="hm-seviesImg1">
-            <img src={img2} className="hm-mark"></img>
-            <div className="hm-text">
-              <ButtonTextandIcon
+            <div className="hm-seviesImg1">
+             <img src={img2} className="hm-mark"></img>
+             <div className="hm-text">
+             <ButtonTextandIcon
                 bttonandiconstyle={"Hm-ButtonServies"}
                 hiedText={false}
                 ShowButtonIcon={false}
@@ -52,10 +46,10 @@ export default function ServiecCardImg({ img1, img2, textheading }) {
                 ButtonText={"Hm-Servies-text"}
                 iconStyle={"HM-button-img"}
               />
+             </div>
             </div>
           </div>
-        </div>
-      </div>
-    </>
+     </div>  
+ </>
   );
 }
